15c17efc33d28f62348ba2dbacabfe52b05bf5b6
Nick536363 <3383371nik@gmail.com>
2026-07-03 01:14:27 +0200

    Added socket creation

--- server.c
+++ server.c
@@ -1,5 +1,22 @@
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
 int main(void)
 {
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_fd == -1){
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
 
+
+    if(close(socket_fd) == -1){
+        perror("close()");
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
